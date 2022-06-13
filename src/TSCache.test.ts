3978123796e2e3180489f923c43e83bc7a6566d0
Fernando Baroni <f@baroni.tech>
2022-06-13 18:02:13 -0300

    Basic tests

--- src/TSCache.test.ts
+++ src/TSCache.test.ts
@@ -1,5 +1,34 @@
-describe('hello', () => {
-    it('world', () => {
-        expect('hello').toBe('world')
+import TSCache from './TSCache';
+
+describe('TSCache', () => {
+    let cache: TSCache
+    beforeEach(() => {
+        cache = new TSCache()
+    })
+
+    it('set and get', () => {
+        cache.set('a', 1)
+        cache.set('b', 2)
+        expect(cache.get('a')).toBe(1)
+        expect(cache.get('b')).toBe(2)
+        expect(cache.get('c')).toBe(null)
+
+        cache.set('a', 3)
+        expect(cache.get('a')).toBe(3)
+    })
+
+    it('set and unset', () => {
+        cache.set('a', 1)
+        expect(cache.get('a')).toBe(1)
+        cache.unset('a')
+        expect(cache.get('a')).toBe(null)
+    })
+    
+    it('numequalto', () => {
+        expect(cache.numequalto(9)).toBe(0)
+        cache.set('a', 1)
+        expect(cache.numequalto(1)).toBe(1)
+        cache.set('b', 1)
+        expect(cache.numequalto(1)).toBe(2)
     })
 })
